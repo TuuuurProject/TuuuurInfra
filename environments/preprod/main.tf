93c040f0394437d9f1fa2700c511d8cdac928678
ElFloww <95478181+ElFloww@users.noreply.github.com>
2026-02-12 17:21:39 +0100

    Fix(): Infra

--- environments/preprod/main.tf
+++ environments/preprod/main.tf
@@ -48,6 +48,20 @@ resource "google_service_account" "run_api" {
   display_name = "${local.prefix} Cloud Run API"
 }
 
+resource "google_secret_manager_secret_iam_member" "api_db_password_access" {
+  project   = var.project_id
+  secret_id = "${local.prefix}-db-password"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.run_api.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "api_redis_auth_access" {
+  project   = var.project_id
+  secret_id = "${local.prefix}-redis-auth"
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.run_api.email}"
+}
+
 module "network" {
   source      = "../../modules/network"
   project_id  = var.project_id
